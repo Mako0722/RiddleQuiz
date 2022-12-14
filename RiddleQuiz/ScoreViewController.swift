753c8b21adcc10c2aa58db4bc72df653bd8b2f88
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-14 22:59:03 +0900

    シャッフル機能をつける、シェア機能をつける、問題文を編集できないようにする

--- RiddleQuiz/ScoreViewController.swift
+++ RiddleQuiz/ScoreViewController.swift
@@ -16,6 +16,13 @@ class ScoreViewController: UIViewController {
         super.viewDidLoad()
         scoreLabel.text = "\(correct)問正解!"
     }
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+        let activityItems = ["\(correct)問正解しました。","#クイズアプリ"]
+        let activityVC = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        self.present(activityVC, animated:true)
+    }
+    
     @IBAction func toTopButtonAction(_ sender: Any) {
         self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
